24b1e76486f0e6eef789ac3b69b9b933d26116b6
Shirin Akhtar <shirinakhtar1998@gmail.com>
2020-12-09 17:17:04 +0530

    commit

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); 
+
+?>
+
+		
+		<div id="main-content"> <!-- Main Content Section with everything -->
+			
+			<noscript> <!-- Show a notification if the user has disabled javascript -->
+				<div class="notification error png_bg">
+					<div>
+						Javascript is disabled or is not supported by your browser. Please <a href="http://browsehappy.com/" title="Upgrade to a better browser">upgrade</a> your browser or <a href="http://www.google.com/support/bin/answer.py?answer=23852" title="Enable Javascript in your browser">enable</a> Javascript to navigate the interface properly.
+					</div>
+				</div>
+			</noscript>
+			
+			<!-- Page Head -->
+			<h2>Welcome Admin</h2>
+			
+			<p id="page-intro">What would you like to do?</p>
+			
+			
+			
+			<div class="clear"></div> <!-- End .clear -->
+			
+			
+			<div class="clear"></div>
+			
+			
+			<!-- Start Notifications -->
+			<!--
+			<div class="notification attention png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Attention notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero. 
+				</div>
+			</div>
+			
+			<div class="notification information png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Information notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>
+			
+			<div class="notification success png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Success notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>
+			
+			<div class="notification error png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Error notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>-->
+			
+			<!-- End Notifications -->
+<?php include('footer.php') ?>
---
+++ admin/order.php
@@ -0,0 +1,151 @@
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); ?>
+<?php
+/**
+ * Template File Doc Comment
+ * 
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+require 'config.php';
+    $error  = array();
+    $message = '';?>
+		
+		<div id="main-content"> <!-- Main Content Section with everything -->
+			
+			<noscript> <!-- Show a notification if the user has disabled javascript -->
+				<div class="notification error png_bg">
+					<div>
+						Javascript is disabled or is not supported by your browser. Please <a href="http://browsehappy.com/" title="Upgrade to a better browser">upgrade</a> your browser or <a href="http://www.google.com/support/bin/answer.py?answer=23852" title="Enable Javascript in your browser">enable</a> Javascript to navigate the interface properly.
+					</div>
+				</div>
+			</noscript>
+			
+			<!-- Page Head -->
+			<h2>Welcome John</h2>
+			<p id="page-intro">What would you like to do?</p>
+			
+			
+			
+			<div class="clear"></div> <!-- End .clear -->
+			
+			<div class="content-box"><!-- Start Content Box -->
+				
+				<div class="content-box-header">
+					
+					<h3>Content box</h3>
+					
+					<ul class="content-box-tabs">
+						<li><a href="#tab1" class="default-tab">Manage</a></li> <!-- href must be unique and match the id of target div -->
+						
+					</ul>
+					
+					<div class="clear"></div>
+					
+				</div> <!-- End .content-box-header -->
+				
+				<div class="content-box-content">
+					
+					<div class="tab-content default-tab" id="tab1"> <!-- This is the target div. id must match the href of this div's tab -->
+					<table>
+					<?php  
+                        $sql = 'SELECT * FROM order_tbl';
+                        $result = $conn->query($sql);
+                        
+                    ?>	
+							<thead>
+								<tr>
+								  
+								   <th>Order Id</th>
+								   <th>User Id</th>
+								   <th>Cart Data</th>
+								   <th>Total</th>
+								   <th>Status</th>
+								   <th>Date & Time</th>
+								   <th>Action</th>
+								</tr>
+								
+							</thead>						 
+							<tbody>
+							<?php
+				while ($row = $result->fetch_assoc()) {
+                    
+        ?>  
+								<tr>
+									
+								<td><?php echo $row["order_id"]; ?></td>
+									<td><?php echo $row["user_id"];?></td>
+									<td><?php echo $row["cartdata"];?></td>
+									<td><?php echo $row["total"];?></td>
+									<td><?php echo $row["status"];?></td>
+									<td><?php echo $row["date"];?></td>
+									<td>
+										<!-- Icons -->
+										 <a href="#" title="Edit"><img src="resources/images/icons/pencil.png" alt="Edit" /></a>
+										 <a href="delete4.php?action=delete&id=<?php echo $row["order_id"];?>" title="Delete"><img src="resources/images/icons/cross.png" alt="Delete" /></a> 
+										 
+									</td>
+								</tr>
+							
+							</tbody>
+							<?php  
+                
+        } $conn->close();?> 
+							
+						</table>
+						
+					</div> <!-- End #tab1 -->
+					
+					
+					
+						<br><br><br><br><br><br><br><br><br><br><br><br>
+						
+					      
+					
+				</div> <!-- End .content-box-content -->
+				
+			</div> <!-- End .content-box -->
+			
+			
+				
+			</div> <!-- End .content-box -->
+			<div class="clear"></div>
+			
+			
+			<!-- Start Notifications -->
+			<!--
+			<div class="notification attention png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Attention notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero. 
+				</div>
+			</div>
+			
+			<div class="notification information png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Information notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>
+			
+			<div class="notification success png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Success notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>
+			
+			<div class="notification error png_bg">
+				<a href="#" class="close"><img src="resources/images/icons/cross_grey_small.png" title="Close this notification" alt="close" /></a>
+				<div>
+					Error notification. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vulputate, sapien quis fermentum luctus, libero.
+				</div>
+			</div>-->
+			
+			<!-- End Notifications -->
+<?php include('footer.php') ?>
